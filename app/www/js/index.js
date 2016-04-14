5414aea4e9a2ee4732c2446ac74f77e85343ec42
Kamikaze <leakleass@protonmail.com>
2016-04-14 12:34:20 +0200

    ddd

--- app/www/js/index.js
+++ app/www/js/index.js
@@ -22,17 +22,11 @@ var app = {
         this.bindEvents();
 
     },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
+    
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
+    
     onDeviceReady: function() {
         //app.receivedEvent('deviceready');
         app.findContacts();
