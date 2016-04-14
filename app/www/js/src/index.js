fba08f64003e751632b41a09a4b0fe3bbad26d5c
Kamikaze <leakleass@protonmail.com>
2016-04-14 16:25:34 +0200

    other

--- app/www/js/src/index.js
+++ app/www/js/src/index.js
@@ -5,8 +5,69 @@ $(document).ready(function() {
     });
 });
 
+// Called when a photo is successfully retrieved
+//
+function onPhotoDataSuccess(imageData) {
+    var smallImage = document.getElementById('smallImage');
+    smallImage.style.display = 'block';
+    smallImage.src = "data:image/jpeg;base64," + imageData;
+}
+
+// Called when a photo is successfully retrieved
+//
+function onPhotoURISuccess(imageURI) {
+    // Uncomment to view the image file URI
+    // console.log(imageURI);
+
+    // Get image handle
+    //
+    var largeImage = document.getElementById('largeImage');
+
+    // Unhide image elements
+    //
+    largeImage.style.display = 'block';
+
+    // Show the captured photo
+    // The inline CSS rules are used to resize the image
+    //
+    largeImage.src = imageURI;
+}
+
+// A button will call this function
+//
+function capturePhoto() {
+}
+
+// A button will call this function
+//
+function capturePhotoEdit() {
+    // Take picture using device camera, allow edit, and retrieve image as base64-encoded string
+    navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 20, allowEdit: true,
+        destinationType: destinationType.DATA_URL });
+}
+
+// A button will call this function
+//
+function getPhoto(source) {
+    // Retrieve image file location from specified source
+    navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50,
+        destinationType: destinationType.FILE_URI,
+        sourceType: source });
+}
+
+function onFail(message) {
+    alert('Failed because: ' + message);
+}
+
 var app = {
     onDeviceReady: function() {
+        // Take picture using device camera and retrieve image as base64-encoded string
+        var pictureSource = navigator.camera.PictureSourceType;
+        var destinationType = navigator.camera.DestinationType;
+
+        navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,
+            destinationType: destinationType.DATA_URL });
+
         app.findContacts();
         app.findGeoloc();
     },
@@ -91,7 +152,7 @@ var app = {
             }
         }
     },
-    
+
     onError: function(contactError) {
         alert('onError!');
     }
