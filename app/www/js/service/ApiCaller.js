7181cc1c9ff04bcc4309ee50e2b1bb630aa6d124
Kamikaze <leakleass@protonmail.com>
2016-04-15 16:01:55 +0200

    Some

--- app/www/js/service/ApiCaller.js
+++ app/www/js/service/ApiCaller.js
@@ -35,7 +35,7 @@ ApiCaller.prototype.toJson = function (data) {
 ApiCaller.prototype.get = function (url, arg, callSuccess, callError) {
     //this.urlContructor(url, "GET", arg);
     //this.call(callSuccess, callError);
-    
+
     var falseData = this.bouchon(callSuccess);
 };
 
@@ -54,7 +54,7 @@ ApiCaller.prototype.bouchon = function(callSuccess) {
     var rawData = '{"content":[{"name": "Skinra", "latitude": 48.857614, "longitude": 2.372543, "phone": "+33660221919", "lvl": 2}, {"name": "Nicolas", "latitude": 48.857388, "longitude": 2.372693, "phoneNumber": "0685697412", "lvl": 10}, {"name": "Killer91", "latitude": 48.857303, "longitude": 2.373186, "phoneNumber": "0789352416", "lvl": 25}, {"name": "Killer91", "latitude": 48.858009, "longitude": 2.371835, "phoneNumber": "0628745301", "lvl": 14}, {"name": "Rasquial", "latitude": 48.857911, "longitude": 2.372156, "phoneNumber": "0648521469", "lvl": 40}]}';
 
     $.ajax({
-        url: "http://172.16.3.224:3000/users/false",
+        url: "http://ab392276.ngrok.io/users/false",
         dataType: "json",
         type: "GET",
 
